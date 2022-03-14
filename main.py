5926008d00bac98e5235785a73405525208b8cde
Marcelopedroni <marcelopedronidasilva@gmail.com>
2022-03-13 23:39:17 -0300

    few initial fixes

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-from secrets import *
-
 from secrets import ACCESS_TOKEN, ACCESS_TOKEN_SECRET, CONSUMER_KEY, CONSUMER_SECRET
 import tweepy
 
 
 BRAZIL_WOE_ID = 23424768
 
-auth = tweepy.OAuthHandler
+
 
